009c945364cb2af519ccad445ea4b3ea6f8e1af7
AgungAwan <agungkristiawan24@gmail.com>
2023-12-03 14:31:58 +0800

    feat: add load env

--- main.go
+++ main.go
@@ -4,16 +4,17 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/kitsune2411/ginCRUD/config"
 )
 
 func main() {
 	app := gin.Default()
+	config.LoadEnv()
 
 	app.GET("/", func(ctx *gin.Context) {
 		ctx.JSON(http.StatusOK, gin.H{
 			"message": "hellow hellow",
 		})
 	})
-
 	app.Run()
 }
